b3f481675214f3f469821da8dc009e5dfda102eb
chrissierraw <christopher.sierra@api-ux.com>
2022-05-19 14:25:42 -0400

    helper class with cookies methods

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@angular/core';
-
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
 
-  
 
-  constructor() { }
+
+  constructor(private cookieService: CookieService) { }
+
+  setCookie(cookieName: string, cookieValue: any, cookiePath = environment.COOKIE_PATH){
+    this.cookieService.set(cookieName, cookieValue, 
+      { 
+        path: cookiePath,
+        expires: new Date(new Date().setFullYear(new Date().getFullYear() + 1)),
+        secure: true
+      });
+
+  }
+
+  removeCookie(cookieName: string, cookiePath = environment.COOKIE_PATH){
+    this.cookieService.delete(cookieName, cookiePath )
+  }
+
+  getCookie(cookieName: string){
+    return this.cookieService.get(cookieName);
+  }
 }

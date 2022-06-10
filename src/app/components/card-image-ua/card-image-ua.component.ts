fc104e454d871cf598bc0ccec742b8e4f8d9e1cc
Christopher Sierra <christopher.sierra@fashionspark.com>
2022-06-10 09:19:09 -0400

    desktop view user area ready

--- src/app/components/card-image-ua/card-image-ua.component.ts
+++ src/app/components/card-image-ua/card-image-ua.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-image-ua',
   templateUrl: './card-image-ua.component.html',
   styleUrls: ['./card-image-ua.component.scss']
 })
-export class CardImageUaComponent implements OnInit {
-
-  constructor() { }
+export class CardImageUaComponent implements OnInit, AfterContentInit {
+  @Input() heightInput: string = '48%'
+  @Input() urlImg: string = '../../../assets/Images/banner-ua2.png'
+  constructor(private elRef: ElementRef) { }
 
   ngOnInit(): void {
+    
+  }
+
+  ngAfterContentInit(){
+    this.elRef.nativeElement.style.setProperty('--imagen-url', this.urlImg)
   }
 
 }

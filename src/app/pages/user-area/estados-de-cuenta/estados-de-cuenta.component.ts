728bfe6087e6381cacce20212d5a9ffcc4cf7f77
Francisco Catalan <francisco.catalan@fashionspark.com>
2022-06-16 10:39:09 -0400

    Integracion del servicio para mostrar el listado de estado de cuentas, en espera de correccion para la columna del mes y el boton descargar. Aun falta el servicio para obtener el pdf

--- src/app/pages/user-area/estados-de-cuenta/estados-de-cuenta.component.ts
+++ src/app/pages/user-area/estados-de-cuenta/estados-de-cuenta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from './../../../services/customers.service';
+import { Period, Item } from '../../../interfaces/periods.interface';
 
 @Component({
   selector: 'app-estados-de-cuenta',
@@ -6,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estados-de-cuenta.component.scss']
 })
 export class EstadosDeCuentaComponent implements OnInit {
+  items: Item[] = []
+  periods: Period = {
+    items: this.items
+  };
+  hola: any;
+  constructor(
+    private customersService: CustomersService
+    ) { }
+    public useIcon: boolean = true;
+  ngOnInit(
+    ): void {
+      // this.onStateAccount('11855134', '6')
+      this.customersService.getAccountStatementPeriods('11855134', '6').subscribe((data: any) =>{
+        this.items = data;
+        // console.log(this.items[3].nombre)
+      })
+    }
 
-  constructor() { }
-  public useIcon: boolean = true;
-  ngOnInit(): void {
+    onDate(){
+
+      // this.items.map(item => item.fecha_estado_cuenta = new Date())
+      // new Date(this.items.map(item => item.fecha_estado_cuenta = ))
+      this.customersService.getAccountStatementPeriods('11855134', '6').subscribe((data: any) =>(
+        this.items = data
+      ));
+    }
   }
-}

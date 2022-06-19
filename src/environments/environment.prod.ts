014b53cfa67885a5fd054c90525f4289129afc8f
Christopher Sierra <christopher.sierra@fashionspark.com>
2022-06-18 21:56:41 -0400

    Update env with login dataa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  TOKEN_ENDPOINT: "http://identity.mobile.qa.grupochien.local:9443/oauth2/token",
-  AUTHORIZE_ENDPOINT: "http://identity.mobile.qa.grupochien.local:9443/oauth2/authorize",
+  TOKEN_ENDPOINT: "http://sister:9443/oauth2/token",
+  AUTHORIZE_ENDPOINT: "http://sister:9443/oauth2/authorize",
   RESPONSE_TYPE: "code",
   SCOPE: "openid",
   REDIRECT_URI: "https://christophersierradev.xyz/auth/login", // https://localhost:9000/login
@@ -9,7 +9,7 @@ export const environment = {
   CLIENT_SECRET: "mThgepHTwQYwlOO2W_qwhfDfkT8a",
   GRANT_TYPE: "authorization_code",
   CLIENT_URL: "https://christophersierradev.xyz/",
-  LOGOUT_URL: "http://identity.mobile.qa.grupochien.local:9443/oidc/logout",
+  LOGOUT_URL: "http://sister:9443/oidc/logout",
   COOKIE_PATH: "/",
   API_URL: "https://api.assi.cl/tfp"
 };

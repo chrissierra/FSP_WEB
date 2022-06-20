1094cf9e35c02e523ba2d1cdc7b413c32e4580d9
Christopher Sierra <christopher.sierra@fashionspark.com>
2022-06-19 22:56:11 -0400

    update env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
-  TOKEN_ENDPOINT: "https://sister:9443/oauth2/token",
-  AUTHORIZE_ENDPOINT: "https://sister:9443/oauth2/authorize",
+  TOKEN_ENDPOINT: "https://identity.mobile.qa.grupochien.local/:9443/oauth2/token",
+  AUTHORIZE_ENDPOINT: "https://identity.mobile.qa.grupochien.local/:9443/oauth2/authorize",
   RESPONSE_TYPE: "code",
   SCOPE: "openid",
   REDIRECT_URI: "https://christophersierradev.xyz/auth/login", // https://localhost:9000/login
-  CLIENT_ID: "RRwIKf1UfCySP1QGvmFVU1kqWZsa",
-  CLIENT_SECRET: "Dj44NbNZD9tFHPaKHU900bsjtmka",
+  CLIENT_ID: "aIS2mMCLA75T41XyblnfqPz2UCca",
+  CLIENT_SECRET: "mThgepHTwQYwlOO2W_qwhfDfkT8a",
   GRANT_TYPE: "authorization_code",
   CLIENT_URL: "https://christophersierradev.xyz/",
-  LOGOUT_URL: "https://sister:9443/oidc/logout",
+  LOGOUT_URL: "https://identity.mobile.qa.grupochien.local/:9443/oidc/logout",
   COOKIE_PATH: "/",
   API_URL: "https://api.assi.cl/tfp"
 };

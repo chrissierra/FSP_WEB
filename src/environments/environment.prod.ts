2997a7d6864ff55d4530823c495c00ff31dd3516
chrissierraw <christopher.sierra@api-ux.com>
2022-06-19 22:42:25 -0400

    changes env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,25 @@
 export const environment = {
   production: true,
+  TOKEN_ENDPOINT: "https://sister.cl:9443/oauth2/token",
+  AUTHORIZE_ENDPOINT: "https://sister.cl:9443/oauth2/authorize",
+  RESPONSE_TYPE: "code",
+  SCOPE: "openid",
+  REDIRECT_URI: "https://christophersierradev.xyz/auth/login", // https://localhost:9000/login
+  CLIENT_ID: "RRwIKf1UfCySP1QGvmFVU1kqWZsa",
+  CLIENT_SECRET: "Dj44NbNZD9tFHPaKHU900bsjtmka",
+  GRANT_TYPE: "authorization_code",
+  CLIENT_URL: "https://christophersierradev.xyz/",
+  LOGOUT_URL: "https://sister.cl:9443/oidc/logout",
+  COOKIE_PATH: "/",
+  API_URL: "https://api.assi.cl/tfp"
+};
+
+
+
+/* 
+
+export const environment = {
+  production: true,
   TOKEN_ENDPOINT: "https://sister:9443/oauth2/token",
   AUTHORIZE_ENDPOINT: "https://sister:9443/oauth2/authorize",
   RESPONSE_TYPE: "code",
@@ -9,7 +29,9 @@ export const environment = {
   CLIENT_SECRET: "Dj44NbNZD9tFHPaKHU900bsjtmka",
   GRANT_TYPE: "authorization_code",
   CLIENT_URL: "https://christophersierradev.xyz/",
-  LOGOUT_URL: "https://sister:9443/oidc/logout",
+  LOGOUT_URL: "https://sister.cl:9443/oidc/logout",
   COOKIE_PATH: "/",
   API_URL: "https://api.assi.cl/tfp"
 };
+
+*/
